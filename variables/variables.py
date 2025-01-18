177150e2562a4ae565bfc6d0b97bd7da94f39b2e
FRQ23 <142237567+FRQ23@users.noreply.github.com>
2025-01-17 18:50:44 -0800

    5th commit

--- variables/variables.py
+++ variables/variables.py
@@ -1,10 +1,28 @@
-a = 26
+#Defining a variable using snake_case (recommended by official python)
 
-b= 5
-
-c = a + b
-
-print(c)
+full_name = "Fernando"
 
 
+#Concatenation with +
 
+welcome  = "Hi " + " How are you?"
+
+
+
+#F-string concatenation
+
+welcome = f"hi {full_name} how are you?"
+
+
+#Membership Operators (in / not in)
+
+print("Fernando" in welcome)
+print("Fernando" not in welcome)
+
+
+
+#Just to know the convention naming files, variables, functions, classes and constants
+
+#snake_case for files, variables and functions
+#PascalCase for classes
+#UPPER_CASE for constants
